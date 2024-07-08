46f7a7b43a56c2b3b73d6dc5ffdbd97754d67ad2
Sean Sovine <sean.r.sovine@gmail.com>
2024-07-08 13:31:05 -0400

    Some nitpicking

--- src/example.cpp
+++ src/example.cpp
@@ -9,19 +9,16 @@
 #include <vector>
 
 int resultRowCallback( void *NotUsed, int argc, char **argv, char **azColName ) {
-  int i;
-  for ( i = 0; i < argc; i++ ) {
+  for ( int i = 0; i < argc; i++ ) {
     std::cout << azColName[ i ] << " = " << ( argv[ i ] ? argv[ i ] : "NULL" ) << "\n";
   }
   std::cout << "\n";
-
   return 0;
 }
 
 int main() {
   sqlite3 *db;
-  int lastReturnCode;
-  lastReturnCode = sqlite3_open( "books.db", &db );
+  int lastReturnCode = sqlite3_open( "books.db", &db );
 
   // Check for error in opening database.
   if ( lastReturnCode ) {
@@ -32,12 +29,12 @@ int main() {
 
   // clang-format off
   std::vector< std::string > queries = {
-      "create table myTable (Title varchar(256), Author varchar(30), Date smallint, Publisher varchar(30))",
-      "insert into myTable (Title, Author, Date, Publisher) values ('War and Peace', 'Leo Tolstoy', 1869, 'Penguin')",
-	  "insert into myTable (Title, Author, Date, Publisher) values ('Dune', 'Frank Herbert', 1965, 'Penguin Classics')",
-      "select * from myTable",
-	  "delete from myTable",
-	  "drop table myTable"
+    "create table myTable (Title varchar(256), Author varchar(30), Date smallint, Publisher varchar(30))",
+    "insert into myTable (Title, Author, Date, Publisher) values ('War and Peace', 'Leo Tolstoy', 1869, 'Penguin')",
+    "insert into myTable (Title, Author, Date, Publisher) values ('Dune', 'Frank Herbert', 1965, 'Penguin Classics')",
+    "select * from myTable",
+    "delete from myTable",
+    "drop table myTable"
 	};
   // clang-format on
 
@@ -50,7 +47,7 @@ int main() {
     // Check for query error and get message if present.
     if ( lastReturnCode != SQLITE_OK ) {
       std::cout << "Error in query #" << i << ".\n";
-      std::cout << "  " << query.c_str() << "\n.";
+      std::cout << "  " << query.c_str() << ".\n";
       std::cout << "SQL error: " << sqlite3_errmsg( db ) << "\n";
       sqlite3_free( zErrMsg );
       break;
